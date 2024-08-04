f8584667d2a386fad889e5483896f284dbb95922
Utsav Singh <singhutsavofficial@gmail.com>
2024-08-04 23:50:06 +0530

    add missing configure store function

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,5 +1,22 @@
-import { configureStore } from '@reduxjs/toolkit'
-
+import { configureStore, createSlice } from '@reduxjs/toolkit'
+const userState = createSlice({
+  name:'userState',
+  initialState:{
+    userName:"Chipmunk52468",
+    userImage:"/chipmunk.jpg"
+  },
+  reducers:{
+    setUserName: (state,action)=>{
+      state.userName = action.payload
+    },
+    setUserImage: (state,action)=>{
+      state.userImage = action.payload
+    }
+  }
+})
+export const userActions = userState.actions
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    userState:userState.reducer,
+  },
 })
