d68a3329066829a58790c3ad1548ffb51aca3e8c
Utsav Singh <singhutsavofficial@gmail.com>
2024-08-05 19:35:29 +0530

    use primereact and quill to setup a rich text editor

---
+++ frontend/src/components/PostCreator.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { postActions } from '../store'
+import { Editor } from 'primereact/editor';
+import { Card } from './Card'
+import "./PostCreator.css"
+const PostCreator = () => {
+    const postTitle = useSelector((state)=>state.postState.postTitle)
+    const postContent = useSelector((state)=>state.postState.postContent)
+    const dispatch = useDispatch();
+  return (
+    <>
+    <div className="post-creator-container">
+        <Card width="50vw" minHeight="90vh">
+          <div className="post-creator-inner-container">
+              <input type="text" id="title-box"  value={postTitle} onChange={(e)=>{
+                dispatch(postActions.setPostTitle(e.target.value))
+              }}/>
+              <Editor 
+              value={postContent}
+              onTextChange={(e)=> dispatch(postActions.setPostContent(e.htmlValue))}
+              style={{border:'none', maxHeight:'25vh', overflow:'auto', backgroundColor:"var(--tertiary-background)", borderRadius:'0px 0px 8px 8px',
+                boxShadow: "1px 1px 1px var(--primary-shadow)"
+              }}/>
+            </div>
+        </Card>
+    </div>
+    </>
+  )
+}
+
+export default PostCreator
