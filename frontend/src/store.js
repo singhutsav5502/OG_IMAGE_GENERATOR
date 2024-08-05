f5900e2edb2d3d6ff0d066afcab87b7304b373fe
Utsav Singh <singhutsavofficial@gmail.com>
2024-08-05 07:55:47 +0530

    setup post state slice with postTitle and postContent

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -14,9 +14,26 @@ const userState = createSlice({
     }
   }
 })
+const postState = createSlice({
+  name:'postState',
+  initialState:{
+    postTitle:"Preview Title",
+    postContent:"Preview content written inside post",
+  },
+  reducers:{
+    setPostTitle:(state,action)=>{
+      state = action.payload
+    },
+    setPostContent:(state,action)=>{
+      state = action.payload
+    }
+  }
+})
 export const userActions = userState.actions
+export const postActions = postState.actions
 export const store = configureStore({
   reducer: {
     userState:userState.reducer,
+    postState:postState.reducer,
   },
 })
